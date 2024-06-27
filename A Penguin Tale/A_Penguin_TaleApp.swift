1c9ac26c5ef610f556b597f8ffeae8ed087f6d89
P7ngu <60442992+P7ngu@users.noreply.github.com>
2024-06-27 10:14:08 +0200

    Initial Commit

---
+++ A Penguin Tale/A_Penguin_TaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Penguin_TaleApp.swift
+//  A Penguin Tale
+//
+//  Created by Matteo Perotta on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct A_Penguin_TaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Penguin Tale/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A Penguin Tale
+//
+//  Created by Matteo Perotta on 27/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
